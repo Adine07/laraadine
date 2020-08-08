1106c73b29b4e9482c40efa087ca0215fcbf20f5
Adine07 <adinepamungkas07@gmail.com>
2020-08-08 22:24:10 +0700

    wow2 tittle

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>@yield('title') | School WOW</title>
+	<title>@yield('title') | School WOW2</title>
 </head>
 
 <body>
